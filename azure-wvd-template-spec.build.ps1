a228c553abc59fbcc24f83fed35a33ea41be7655
Tyler Doerksen <tylerd@microsoft.com>
2021-01-15 14:12:36 -0600

    adding deployment script

--- azure-wvd-template-spec.build.ps1
+++ azure-wvd-template-spec.build.ps1
@@ -5,14 +5,26 @@
 
 param(
 	[ValidateSet('Debug', 'Release')]
-	[string]$Configuration = 'Release'
+	[string]$Configuration = 'Release',
+	[string]$SpecResourceGroupName = "TemplateSpecs-RG",
+	[string]$Location = "westus2",
+	[string]$Version = "0.1",
+	[string]$VersionSuffix = "local",
+	[bool]$OverwiteIfExists = $true
 )
 
 # Synopsis: Build project.
 task deploy {
 	exec { 
-		dotnet build -c $Configuration 
+		$SpecVersion = 
+
+		New-AzTemplateSpec -ResourceGroupName $SpecResourceGroupName -Location $Location -Name "NetApp" -Version $SpecVersion -TemplateFile ".\arm-templates\NetApp\New-NetApp-Account.json" -Force:$OverwiteIfExists
+	
+		New-AzTemplateSpec -ResourceGroupName $SpecResourceGroupName -Location $Location -Name "wvd-pod" -Version $SpecVersion -TemplateFile ".\arm-templates\wvd-pod\mainTemplate.json" -Force:$OverwiteIfExists
+
+		New-AzTemplateSpec -ResourceGroupName $SpecResourceGroupName -Location $Location -Name "wvd-hosts" -Version $SpecVersion -TemplateFile ".\arm-templates\wvd-hosts\mainTemplate.json" -Force:$OverwiteIfExists
 	}
+
 }
 
 task . {}
